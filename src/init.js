63483e229b9dfd9f1e12f04f076aba28a1804810
Brainketunze <67590602+Brainketunze@users.noreply.github.com>
2020-12-28 11:43:53 +0100

    Update init.js

--- src/init.js
+++ src/init.js
@@ -7,6 +7,15 @@ import './listeners/change-separator.js';
 // does your project have code that executes when the document or window are ready?
 // that code can go here
 
+// import all listener files so they can attach to the DOM
+
+// import './listeners/voiceChanged.js';
+// import './listeners/change.js';
+// import './listeners/changeOption.js';
+// import './listeners/speak.js';
+// import './listeners/stop.js'
+
+
 import { data } from './data.js';
 
 document.getElementById('separator-input').value = data.separator;
